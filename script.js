e0cae0d5ae8b445a9725cf1208b0d7ebccee17cf
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-04-01 21:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 function insert_Row() {
 	const table = document.getElementById("sampleTable");
 
-	// Create new row
 	const newRow = document.createElement("tr");
 
-	// Create cells
 	const cell1 = document.createElement("td");
 	const cell2 = document.createElement("td");
 
-	// Add text
 	cell1.textContent = "New Cell1";
 	cell2.textContent = "New Cell2";
 
-	// Append cells to row
 	newRow.appendChild(cell1);
 	newRow.appendChild(cell2);
 
-	// Append row to table
-	table.appendChild(newRow);
+	const firstRow = table.rows[1];
+	table.insertBefore(newRow, firstRow);
 }
